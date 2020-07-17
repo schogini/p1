f02aad55c3291f727d0bff72e693327c7355f571
Sreeprakash Neelkantan <sree@schogini.com>
2020-07-18 00:07:41 +0530

    Sree

--- t1.sh
+++ t1.sh
@@ -5,7 +5,7 @@ docker run --rm -d --name tmp-web --net tmp-net -e WEB=$1 $2
 qq=$(docker run --rm --net tmp-net curlimages/curl:7.71.1 -s http://tmp-web:8123|grep -c "App Version: $1")
 docker container rm -f tmp-web > /dev/null 2>&1
 if [ $qq -gt 0 ]; then
-	exit 0
+	echo 0
 else
-	exit 1
+	echo 1
 fi
