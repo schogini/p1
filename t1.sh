dc02fde6fe87ac03693dad20994c3e5d153d3793
Sreeprakash Neelkantan <sree@schogini.com>
2020-07-17 16:45:13 +0530

    Sree

---
+++ t1.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+docker network create tmp-net > /dev/null 2>&1
+docker run --rm -d --name tmp-web --net tmp-net -e WEB=$1 $2
+qq=$(docker run --rm --net tmp-net curlimages/curl:7.71.1 -s http://tmp-web:8123|grep -c "App Version: $1")
+docker container rm -f tmp-web > /dev/null 2>&1
+if [ $qq -gt 0 ]; then
+	exit 0
+else
+	exit 1
+fi
