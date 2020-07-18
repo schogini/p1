6f2695925d81bb1164a3626c01bde302c2fb2b2c
Sreeprakash Neelkantan <sree@schogini.com>
2020-07-18 15:53:03 +0530

    Sree

--- t1.sh
+++ t1.sh
@@ -5,7 +5,7 @@ docker run --rm -d --name tmp-web --net tmp-net -e WEB=$1 $2
 qq=$(docker run --rm --net tmp-net curlimages/curl:7.71.1 -s http://tmp-web:8123|grep -c "App Version: $1")
 docker container rm -f tmp-web > /dev/null 2>&1
 if [ $qq -gt 0 ]; then
-	echo 0
+	echo "SUCCESS" > result.txt
 else
-	echo 1
+	echo "FAILED" > result.txt
 fi
