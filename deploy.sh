9f77be256e633e0813396aeb050c35f380a0a310
Sreeprakash Neelkantan <sree@schogini.com>
2020-07-26 18:54:33 +0530

    Sree

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ if [ $qq -gt 0 ]; then
 	docker service update --env-add WEB=$2 --image $1 tmp1-svc
 else
 	echo "Deploying Application.. $1"
-	docker service create --name tmp1-svc --replicas 2 --publish 8080:8123 --env WEB=$2 $1
+	docker service create --name tmp1-svc --replicas 2 --publish 8081:8123 --env WEB=$2 $1
 fi
