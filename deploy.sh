03006daac54e8e3005e66c577127eb42390c8cc2
Sreeprakash Neelkantan <sree@schogini.com>
2020-07-26 18:50:16 +0530

    Sree

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 
-qq=$(docker service ls|grep -c tmp-svc)
+qq=$(docker service ls|grep -c tmp1-svc)
 
 #// --update-delay
 
 if [ $qq -gt 0 ]; then
 	echo "Doing a Rolling Update.. $1 "
-	docker service update --env-add WEB=$2 --image $1 tmp-svc
+	docker service update --env-add WEB=$2 --image $1 tmp1-svc
 else
 	echo "Deploying Application.. $1"
-	docker service create --name tmp-svc --replicas 2 --publish 8080:8123 --env WEB=$2 $1
+	docker service create --name tmp1-svc --replicas 2 --publish 8080:8123 --env WEB=$2 $1
 fi
